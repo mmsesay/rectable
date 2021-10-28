c5c75fba1a759307fc6a7c09eda06431503fb3b4
mmsesay <maej_01@hotmail.com>
2021-10-28 19:32:28 +0000

    Add: project setup

---
+++ src/components/Table.tsx
@@ -0,0 +1,13 @@
+// Table.tsx component
+import React from 'react'
+
+const Table = () => {
+
+    return (
+        <div>
+            <p>Table compoment</p>
+        </div>
+    )
+}
+
+export default Table
